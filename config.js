d99b123408fc06fb0caced04a906ff425af04b1f
Joseph Elalam <joseph.elalam@optile.net>
2020-02-03 11:39:18 +0100

    implement env depending on th hostname

--- config.js
+++ config.js
@@ -1,55 +1,127 @@
-window.attributes = {
-  configuration: {
-    baseURL: "https://api.integration.oscato.com/pci/v1/express",
-    clientId: "v1.opt-div-app.1b293aa844de46d4860da0ad5d29f0fe",
-    country: "US",
-    language: "en_US",
-    translation: [
-      {
-        language: "en",
-        resource: {
-          confirm: "confirm"
-        }
-      },
-      {
-        language: "de",
-        resource: {
-          confirm: "bestätigen"
-        }
-      }
-    ],
-    paymentMethodsConfiguration: [
-      {
-        code: "PAYPAL",
-        style: {
-          size: "small",
-          color: "gold",
-          shape: "rect",
-          label: "checkout"
-        }
-      }
-    ]
-  },
-  createTransactionDetails: function(requestData) {
-    return {
-      transactionId: "tr-" + new Date().getTime(),
+var attributes = {
+  local: {
+    configuration: {
+      baseURL: "https://api.integration.oscato.com/pci/v1/express",
+      clientId: "v1.opt-div-app.1b293aa844de46d4860da0ad5d29f0fe",
       country: "US",
-      providerRequest: requestData,
-      payment: {
-        amount: 20,
-        currency: "USD",
-        reference: "Payment By legacy shop",
-        longReference: {
-          essential: "Thank you for your purchase!"
-        }
-      },
-      products: [
+      language: "en_US",
+      translation: [
         {
-          name: "Ledger Nano S",
-          amount: 20
+          language: "en",
+          resource: {
+            confirm: "confirm"
+          }
+        },
+        {
+          language: "de",
+          resource: {
+            confirm: "bestätigen"
+          }
+        }
+      ],
+      paymentMethodsConfiguration: [
+        {
+          code: "PAYPAL",
+          style: {
+            size: "small",
+            color: "gold",
+            shape: "rect",
+            label: "checkout"
+          }
         }
       ]
-    };
+    },
+    createTransactionDetails: function(requestData) {
+      return {
+        transactionId: "tr-" + new Date().getTime(),
+        country: "US",
+        providerRequest: requestData,
+        payment: {
+          amount: 20,
+          currency: "USD",
+          reference: "Payment By legacy shop",
+          longReference: {
+            essential: "Thank you for your purchase!"
+          }
+        },
+        products: [
+          {
+            name: "Ledger Nano S",
+            amount: 20
+          }
+        ]
+      };
+    },
+    customFunctions: {}
   },
-  customFunctions: {}
+  integration: {
+    configuration: {
+      baseURL: "https://api.integration.oscato.com/pci/v1/express",
+      clientId: "v1.opt-div-app.2e172a46c87245a8804ac938603d5b12",
+      country: "US",
+      language: "en_US",
+      translation: [
+        {
+          language: "en",
+          resource: {
+            confirm: "confirm"
+          }
+        },
+        {
+          language: "de",
+          resource: {
+            confirm: "bestätigen"
+          }
+        }
+      ],
+      paymentMethodsConfiguration: [
+        {
+          code: "PAYPAL",
+          style: {
+            size: "small",
+            color: "gold",
+            shape: "rect",
+            label: "checkout"
+          }
+        }
+      ]
+    },
+    createTransactionDetails: function(requestData) {
+      return {
+        transactionId: "tr-" + new Date().getTime(),
+        country: "US",
+        providerRequest: requestData,
+        payment: {
+          amount: 20,
+          currency: "USD",
+          reference: "Payment By legacy shop",
+          longReference: {
+            essential: "Thank you for your purchase!"
+          }
+        },
+        products: [
+          {
+            name: "Ledger Nano S",
+            amount: 20
+          }
+        ]
+      };
+    },
+    customFunctions: {}
+  }
 };
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+var attr;
+if (window.location.hostname === "localhost") {
+  attr = attributes.local;
+} else {
+  attr = attributes.integration;
+}
